db65d82aa03e3853f7132f03557f8d22d50cf3eb
Martin Tillenius <martin.tillenius@gmail.com>
2024-06-29 23:40:25 +0200

    bettingh closed and first bets up.

---
+++ bets2.js
@@ -0,0 +1,19 @@
+gamblerListTDB.push(new Gambler("mrtn","mrtn","PAID",
+	new Array(1,2,2,1,1,1,0,2),
+	new Array(2,1,0,0,0,0,2,1),
+	new Array("2","1","1","1","1","1","2","1")));
+
+gamblerListTDB.push(new Gambler("sverek","sverek","PAID",
+	new Array(1,2,0,2,1,2,2,1),
+	new Array(2,1,1,0,2,1,1,2),
+	new Array("2","1","2","1","2","1","1","2")));
+
+gamblerListTDB.push(new Gambler("magnus","magnus","PAID",
+	new Array(1,3,2,1,2,1,1,0),
+	new Array(2,1,0,1,1,0,1,2),
+	new Array("2","1","1","X","1","1","X","2")));
+
+gamblerListTDB.push(new Gambler("jens","jens","PAID",
+	new Array(0,0,0,0,0,0,0,0),
+	new Array(0,0,0,0,0,0,0,0),
+	new Array("X","X","X","X","X","X","X","X")));
