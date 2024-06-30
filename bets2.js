9e669923d3b691a458049ab65dc8a6cfe6e53650
Martin Tillenius <martin.tillenius@gmail.com>
2024-06-30 15:30:24 +0200

    more bets

--- bets2.js
+++ bets2.js
@@ -17,3 +17,8 @@ gamblerListTDB.push(new Gambler("jens","jens","PAID",
 	new Array(0,0,0,0,0,0,0,0),
 	new Array(0,0,0,0,0,0,0,0),
 	new Array("X","X","X","X","X","X","X","X")));
+
+gamblerListTDB.push(new Gambler("ergh","ergh","PAID",
+	new Array(0,2,1,2,2,2,0,1),
+	new Array(1,0,0,1,1,0,2,1),
+	new Array("2","1","1","1","1","1","2","X")));
