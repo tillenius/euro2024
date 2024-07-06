eef443108d061d40ef4227588c97263cc3c1e0b3
Martin Tillenius <martin.tillenius@gmail.com>
2024-07-06 23:17:09 +0200

    updated

--- bets2.js
+++ bets2.js
@@ -1,7 +1,7 @@
 gamblerListTDB.push(new Gambler("mrtn","mrtn","PAID",
-	new Array(1,2,2,1,1,1,0,2),
-	new Array(2,1,0,0,0,0,2,1),
-	new Array("2","1","1","1","1","1","2","1")));
+	new Array(1,2,2,1,1,1,0,2,1,1,2,0),
+	new Array(2,1,0,0,0,0,2,1,2,1,0,0),
+	new Array("2","1","1","1","1","1","2","1","2","X","1","X")));
 
 gamblerListTDB.push(new Gambler("sverek","sverek","PAID",
 	new Array(1,2,0,2,1,2,2,1),
@@ -9,9 +9,9 @@ gamblerListTDB.push(new Gambler("sverek","sverek","PAID",
 	new Array("2","1","2","1","2","1","1","2")));
 
 gamblerListTDB.push(new Gambler("magnus","magnus","PAID",
-	new Array(1,3,2,1,2,1,1,0),
-	new Array(2,1,0,1,1,0,1,2),
-	new Array("2","1","1","X","1","1","X","2")));
+	new Array(1,3,2,1,2,1,1,0,1,0,1,1),
+	new Array(2,1,0,1,1,0,1,2,2,2,0,1),
+	new Array("2","1","1","X","1","1","X","2","2","2","1","X")));
 
 gamblerListTDB.push(new Gambler("jens","jens","PAID",
 	new Array(0,0,0,0,0,0,0,0),
